ee1fb2788fa59e41e5c9a126bca00c9e08d0e60e
Joshua Sims <JoshuaSimsVA@gmail.com>
2016-09-29 12:30:19 -0400

    Resolves #11, resolves #10, resolves #16, resolves #5, and resolves #13

---
+++ Pong/Assets/scripts/GoalZoneConsumesBall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalZoneConsumesBall : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject ball = collision.gameObject;
+
+        ball.transform.position = new Vector3(0, (float)0.25, 0);
+
+        ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+
+        ball.GetComponent<BallBehaviour>().Invoke("InitiateBallMovement", 1);
+    }
+}
