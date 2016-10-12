4265128cb6c8317671011201e352eac87fb06bba
EvanArroyo <earroyo244@gmail.com>
2016-10-12 15:47:00 -0400

    Resolves #19
    Resolves #19 Still has partial bugs to be worked out but points do increment for both players once they enter the goal zone.

---
+++ PaddleBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleBehaviour : MonoBehaviour
+{
+    public PaddleUser thisPaddleUser;
+
+    public int paddleSpeed;
+
+    void Start()
+    {
+        if (thisPaddleUser == PaddleUser.AI)
+        {
+            Rigidbody AIPaddle = GetComponent<Rigidbody>();
+
+            AIPaddle.AddForce(0, 0, paddleSpeed * AIPaddle.mass,
+                ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float userInput = 0;
+
+        switch (thisPaddleUser)
+        {
+            case PaddleUser.Player01:
+                userInput = Input.GetAxis("Player01");
+                break;
+            case PaddleUser.Player02:
+                userInput = Input.GetAxis("Player02");
+                break;
+        }
+
+        transform.position += new Vector3(0, 0, userInput * paddleSpeed *
+            Time.deltaTime);
+    }
+}
+
+public enum PaddleUser
+{
+    Player01, Player02, AI
+}
---
+++ ScoreUIPlayerTwo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreUIPlayerTwo : MonoBehaviour
+{
+    public Text ScoreBoardO;
+    GameObject ball;
+    private int scoreTwo = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ball.transform.position.x >= 9.42f)
+        {
+            scoreTwo++;
+        }
+
+        ScoreBoardO.text = "Player Two: " + scoreTwo.ToString();
+        print("Player Two: " + scoreTwo);
+    }
+}
