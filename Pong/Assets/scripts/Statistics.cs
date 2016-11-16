0aebd63ed66e0776f77943f57a588ab4c0c7a3c9
Joshua Sims <JoshuaSimsVA@gmail.com>
2016-11-16 04:56:46 -0500

    goal-zone-left-right-statistics

---
+++ Pong/Assets/scripts/Statistics.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class Statistics : MonoBehaviour
+{
+    public static Statistics statistics;
+
+    public int wins;
+
+    void Awake()
+    {
+        if (statistics == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            statistics = this;
+        }
+        else if (statistics != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Save()
+    {
+        FileStream statisticsFile = File.Open(
+            Application.persistentDataPath + "/statistics.dat", 
+            FileMode.OpenOrCreate);
+
+        StatisticsSerializable statisticsSerializable = 
+            new StatisticsSerializable();
+        statisticsSerializable.wins = wins;
+        
+        BinaryFormatter serializer = new BinaryFormatter();
+        serializer.Serialize(statisticsFile, statisticsSerializable);
+
+        statisticsFile.Close();       
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/statistics.dat"))
+        {
+            FileStream statisticsFile = File.Open(
+                Application.persistentDataPath + "/statistics.dat", 
+                FileMode.Open);
+
+            BinaryFormatter deserializer = new BinaryFormatter();
+
+            StatisticsSerializable statisticsSerializable =
+                (StatisticsSerializable) deserializer.Deserialize(
+                statisticsFile);
+            statisticsFile.Close();
+        }
+    }
+
+    [Serializable]
+    private class StatisticsSerializable
+    {
+        internal int wins;
+    }
+}
