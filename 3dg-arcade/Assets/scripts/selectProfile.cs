c23ca34dd41d7bcaa74ca80726ffb6e40d93bc59
Joshua Sims <JoshuaSimsVA@gmail.com>
2016-11-30 16:37:19 -0500

    choose-a-player-02

---
+++ 3dg-arcade/Assets/scripts/selectProfile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+internal class selectProfile : MonoBehaviour
+{
+    Dropdown dropDown;
+
+    private void Start()
+    {
+        dropDown = this.gameObject.GetComponent<Dropdown>();
+    }
+
+    internal void select()
+    {
+        string profileChoice = this.gameObject.GetComponent<Text>().text;
+        if (profileChoice.Equals("<create profile>"))
+        {
+
+        }
+    }
+}
