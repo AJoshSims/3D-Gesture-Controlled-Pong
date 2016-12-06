38f14aa7ff8c912bbec48378db13dc14b357e99f
Evan Arroyo <Evan Arroyo>
2016-12-06 17:26:25 -0500

    added cursor but doesnt click also added quit button and back button

---
+++ 3dg-arcade/Assets/scripts/CursorMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class CursorMovement : MonoBehaviour
+{
+    private GameObject RightHand;
+
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (RightHand == null)
+        {
+            RightHand = GameObject.Find("HandRight");
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(RightHand.transform.position.x, RightHand.transform.position.y,
+                RightHand.transform.position.z);
+        }
+    }
+}
