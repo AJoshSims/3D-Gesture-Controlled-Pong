9a0e55e065441c4e58be31c1e804642c215aecf2
Evan Arroyo <Evan Arroyo>
2016-12-13 12:59:55 -0500

    quick-fix-02

--- 3dg-arcade/Assets/scripts/GoalZoneConsumesBall.cs
+++ 3dg-arcade/Assets/scripts/GoalZoneConsumesBall.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GoalZoneConsumesBall : MonoBehaviour
 {
@@ -43,91 +44,149 @@ public class GoalZoneConsumesBall : MonoBehaviour
         thisGoalZoneX = transform.position.x;
         thisGoalZoneZ = transform.position.z;
 
-        player01ScoreDisplay.text = "Player One: " + player01Goals;
-        player02ScoreDisplay.text = "Player Two: " + player02Goals;
+        player01ScoreDisplay.text = Settings.settings.getProfileNamePlayerOne() + ": " + player01Goals;
+        player02ScoreDisplay.text = Settings.settings.getProfileNamePlayerTwo() + ": " + player02Goals;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         GameObject ball = collision.gameObject;
 
+        ResetBall(ball);
+
         if (thisGoalZoneX == goalZone02X)
         {
-            ++player01Goals;
-            ++Statistics.statistics.pongPlayer01WinGoals;
-            ++Statistics.statistics.pongPlayer02LossGoals;
+            Statistics.statistics.UpdateStatisticIndependent(
+                Settings.settings.getProfileIndexPlayerOne(),
+                Statistics.winGoals,
+                1);
+            Statistics.statistics.UpdateStatisticIndependent(
+                Settings.settings.getProfileIndexPlayerTwo(),
+                Statistics.lossGoals,
+                1);
 
             if (thisGoalZoneZ == goalZone02RightZ)
             {
-                ++Statistics.statistics.pongPlayer01WinGoalsLeft;
-                ++Statistics.statistics.pongPlayer02LossGoalsRight;
+                ++player01Goals;
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerOne(),
+                    Statistics.winGoalsLeft,
+                    1);
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerTwo(),
+                    Statistics.lossGoalsRight,
+                    1);
             }
 
             else if (thisGoalZoneZ == goalZone02MiddleZ)
             {
-                ++Statistics.statistics.pongPlayer01WinGoalsMiddle;
-                ++Statistics.statistics.pongPlayer02LossGoalsMiddle;
+                ++player01Goals;
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerOne(),
+                    Statistics.winGoalsMiddle,
+                    1);
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerTwo(),
+                    Statistics.lossGoalsMiddle,
+                    1);
             }
 
             else if (thisGoalZoneZ == goalZone02LeftZ)
             {
-                ++Statistics.statistics.pongPlayer01WinGoalsRight;
-                ++Statistics.statistics.pongPlayer02LossGoalsLeft;
+                ++player01Goals;
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerOne(),
+                    Statistics.winGoalsRight,
+                    1);
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerTwo(),
+                    Statistics.lossGoalsLeft,
+                    1);
             }
 
             if ((player01Goals % 5) == 0)
             {
-                ++Statistics.statistics.pongPlayer01Wins;
-                ++Statistics.statistics.pongPlayer02Losses;
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerOne(),
+                    Statistics.wins,
+                    1);
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerTwo(),
+                    Statistics.losses,
+                    1);
+
+                SceneManager.LoadScene(1, LoadSceneMode.Single);
             }
 
-            // TODO restore
-            //player01ScoreDisplay.text =
-            //    "Player One: " + player01Goals;
+            player01ScoreDisplay.text = Settings.settings.getProfileNamePlayerOne() + ": " + player01Goals;
         }
 
         else if (thisGoalZoneX == goalZone01X)
         {
-            ++player02Goals;
-            ++Statistics.statistics.pongPlayer02WinGoals;
-            ++Statistics.statistics.pongPlayer01LossGoals;
+            Statistics.statistics.UpdateStatisticIndependent(
+                Settings.settings.getProfileIndexPlayerTwo(),
+                Statistics.winGoals,
+                1);
+            Statistics.statistics.UpdateStatisticIndependent(
+                Settings.settings.getProfileIndexPlayerOne(),
+                Statistics.lossGoals,
+                1);
 
             if (thisGoalZoneZ == goalZone01RightZ)
             {
-                ++Statistics.statistics.pongPlayer02WinGoalsLeft;
-                ++Statistics.statistics.pongPlayer01LossGoalsRight;
+                ++player02Goals;
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerTwo(),
+                    Statistics.winGoalsLeft,
+                    1);
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerOne(),
+                    Statistics.lossGoalsRight,
+                    1);
             }
 
             else if (thisGoalZoneZ == goalZone01MiddleZ)
             {
-                ++Statistics.statistics.pongPlayer02WinGoalsMiddle;
-                ++Statistics.statistics.pongPlayer01LossGoalsMiddle;
+                ++player02Goals;
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerTwo(),
+                    Statistics.winGoalsMiddle,
+                    1);
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerOne(),
+                    Statistics.lossGoalsMiddle,
+                    1);
             }
 
             else if (thisGoalZoneZ == goalZone01LeftZ)
             {
-                ++Statistics.statistics.pongPlayer02WinGoalsRight;
-                ++Statistics.statistics.pongPlayer01LossGoalsLeft;
+                ++player02Goals;
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerTwo(),
+                    Statistics.winGoalsRight,
+                    1);
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerOne(),
+                    Statistics.lossGoalsLeft,
+                    1);
             }
 
             if ((player02Goals % 5) == 0)
             {
-                ++Statistics.statistics.pongPlayer02Wins;
-                ++Statistics.statistics.pongPlayer01Losses;
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerTwo(),
+                    Statistics.wins,
+                    1);
+                Statistics.statistics.UpdateStatisticIndependent(
+                    Settings.settings.getProfileIndexPlayerOne(),
+                    Statistics.losses,
+                    1);
+
+                SceneManager.LoadScene(1, LoadSceneMode.Single);
             }
 
-            // TODO restore
-            //player02ScoreDisplay.text =
-            //    "Player Two: " + player02Goals;
+            player02ScoreDisplay.text = Settings.settings.getProfileNamePlayerTwo() + ": " + player02Goals;
         }
-
-        ResetBall(ball);
-
-        // TODO remove
-        player01ScoreDisplay.text =
-            "Player One: " + player01Goals;
-        player02ScoreDisplay.text =
-            "Player Two: " + player02Goals;
 
         Statistics.statistics.Save();
     }
---
+++ 3dg-arcade/Assets/scripts/PopulateStatisticsView.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PopulateStatisticsView : MonoBehaviour
+{
+    private Dropdown thisDropdown;
+
+    public GameObject statisticsViewContent;
+
+    public GameObject statisticText;
+
+    private Text[] statisticTexts;
+
+    private void Start()
+    {
+        thisDropdown = gameObject.GetComponent<Dropdown>();
+
+        string profileToPlace = null;
+        for (
+            int i = 0;
+            i < Settings.profilesMax;
+            ++i)
+        {
+            profileToPlace = Settings.settings.getProfileName(i);
+            if (profileToPlace.Equals("<create profile>"))
+            {
+                profileToPlace = "unavailable";
+            }
+
+            thisDropdown.options[i] = new Dropdown.OptionData(profileToPlace);
+        }
+
+        thisDropdown.value = 0;
+        thisDropdown.captionText.text = thisDropdown.options[0].text;
+
+        Statistics.statistics.CalculateStatisticsDependent(0);
+
+        GameObject statistic = null;
+        float changeBy = 0;
+        statisticTexts = 
+            new Text[
+            Statistics.statisticsIndependentNum + 
+            Statistics.statisticsDependentNum];
+        Text statisticTextText = null;
+        int statisticTextsIndex = 0;
+        for (
+            int statisticIndex = 0;
+            statisticIndex < Statistics.statisticsIndependentNum;
+            ++statisticIndex)
+        {
+            statistic = Instantiate(statisticText);
+            statistic.transform.SetParent(statisticsViewContent.transform, false);
+            statistic.transform.position =
+                new Vector3(
+                statistic.transform.position.x,
+                statistic.transform.position.y - changeBy,
+                statistic.transform.position.z);
+            changeBy = changeBy + 30;
+            statisticTextText = statistic.GetComponent<Text>();
+            statisticTexts[statisticTextsIndex] = statisticTextText;
+            statisticTextText.text =
+                Statistics.statistics.
+                statisticsIndependentNames[statisticIndex] + ": " + Statistics.statistics.getStatisticIndependent(0, statisticIndex);
+
+            ++statisticTextsIndex;
+        }
+
+        for (
+            int statisticIndex = 0;
+            statisticIndex < Statistics.statisticsDependentNum;
+            ++statisticIndex)
+        {
+            statistic = Instantiate(statisticText);
+            statistic.transform.SetParent(statisticsViewContent.transform, false);
+            statistic.transform.position =
+                new Vector3(
+                statistic.transform.position.x,
+                statistic.transform.position.y - changeBy,
+                statistic.transform.position.z);
+            changeBy = changeBy + 30;
+            statisticTextText = statistic.GetComponent<Text>();
+            statisticTexts[statisticTextsIndex] = statisticTextText;
+            statisticTextText.text =
+                Statistics.statistics.
+                statisticsDependentNames[statisticIndex] + ": " + Statistics.statistics.getStatisticDependent(0, statisticIndex);
+
+            ++statisticTextsIndex;
+        }
+    }
+
+    void Select()
+    {
+        int statisticTextsIndex = 0;
+
+        if (thisDropdown.captionText.text.Equals("unavailable"))
+        {
+            for (
+                int i = 0;
+                i < Statistics.statisticsIndependentNum;
+                ++i)
+            {
+                statisticTexts[statisticTextsIndex].text = Statistics.statistics.
+                    statisticsIndependentNames[i] + ": ";
+
+                ++statisticTextsIndex;
+            }
+
+            for (
+                int i = 0;
+                i < Statistics.statisticsDependentNum;
+                ++i)
+            {
+                statisticTexts[statisticTextsIndex].text = Statistics.statistics.
+                    statisticsIndependentNames[i] + ": ";
+
+                ++statisticTextsIndex;
+            }
+
+
+        }
+
+        else
+        {
+            Statistics.statistics.CalculateStatisticsDependent(thisDropdown.value);
+
+            for (
+                int i = 0;
+                i < Statistics.statisticsIndependentNum;
+                ++i)
+            {
+                statisticTexts[statisticTextsIndex].text = Statistics.statistics.
+                    statisticsIndependentNames[i] + ": " + Statistics.statistics.getStatisticIndependent(thisDropdown.value, i);
+
+                ++statisticTextsIndex;
+            }
+
+            for (
+                int i = 0;
+                i < Statistics.statisticsDependentNum;
+                ++i)
+            {
+                statisticTexts[statisticTextsIndex].text = Statistics.statistics.
+                    statisticsIndependentNames[i] + ": " + Statistics.statistics.getStatisticDependent(thisDropdown.value, i);
+
+                ++statisticTextsIndex;
+            }
+        }
+    }
+}
