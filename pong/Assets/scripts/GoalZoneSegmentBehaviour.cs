3fe4c36ae789b761be7d2d9eb4d3a13862b15699
Joshua Sims <JoshuaSimsVA@gmail.com>
2017-02-05 18:35:59 -0500

    adjust-ball-and-goal-zone-segment-interaction-01

---
+++ pong/Assets/scripts/GoalZoneSegmentBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalZoneSegmentBehaviour : MonoBehaviour
+{
+    private bool ableToConsumeBall;
+
+    internal void setAbleToConsumeBall(bool ableToConsumeBall)
+    {
+        this.ableToConsumeBall = ableToConsumeBall;
+    }
+
+    internal bool isAbleToConsumeBall()
+    {
+        return ableToConsumeBall;
+    }
+
+	void Start ()
+    {
+        setAbleToConsumeBall(true);
+	}
+}
