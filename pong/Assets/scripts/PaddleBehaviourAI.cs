131dcd5060cff13f3a7906596220b2c4f37040b8
Joshua Sims <JoshuaSimsVA@gmail.com>
2017-02-07 06:18:11 -0500

    adjust-artificial-intelligence-01

---
+++ pong/Assets/scripts/PaddleBehaviourAI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBehaviourAI : MonoBehaviour
+{
+    // Change this because we need to accomodate multiple balls.
+    // We are going to need a queue.
+    public GameObject ball;
+
+    public enum ArtificialIntelligenceLevel
+    {
+        High, Medium, Low
+    }
+
+    public ArtificialIntelligenceLevel artificalIntelligenceLevelSelected;
+
+    private float speed;
+
+    void Start ()
+    {
+        switch (artificalIntelligenceLevelSelected)
+        {
+            case ArtificialIntelligenceLevel.High:
+                speed = 50;
+                break;
+            case ArtificialIntelligenceLevel.Medium:
+                speed = 30;
+                break;
+            case ArtificialIntelligenceLevel.Low:
+                speed = 20;
+                break;
+        }
+
+        transform.localPosition = new Vector3(0, 0, 30);
+	}
+	
+	void Update ()
+    {
+        if (transform.localPosition.z > 2)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                ball.transform.position,
+                speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.localPosition = Vector3.MoveTowards(
+                transform.position,
+                new Vector3(0, 0, 30),
+                speed * Time.deltaTime);
+        }
+	}
+}
---
+++ pong/Assets/scripts/PaddleBehaviourPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Windows.Kinect;
+
+public class PaddleBehaviourPlayer : MonoBehaviour
+{
+    private GameObject rightHand;
+
+    void Update()
+    {
+        if (rightHand == null)
+        {
+            rightHand = GameObject.Find("HandRight");
+        }
+
+        else
+        {
+            gameObject.transform.position = new Vector3
+                (rightHand.transform.position.x, 
+                rightHand.transform.position.y, 
+                rightHand.transform.position.z);
+        }
+    }
+}
